480b87ddb51cf31b78a94e8dfead94a8b7be40ae
leandrotune <leandro.tune@hotmail.com>
2024-03-22 22:44:12 -0300

    chore: add colors used in the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: '#FFFFFF',
+        blue: {
+          light: '#8FB2F5',
+        },
+        gray: {
+          100: '#FAFAFA',
+          200: '#BFBFD4',
+          300: '#ABABC4',
+          400: '#7F7F98',
+          500: '#3B3B54',
+          600: '#22222F',
+          700: '#1C1C27',
+          800: '#16161F',
+          900: '#13131A',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
