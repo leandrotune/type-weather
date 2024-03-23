16f1db8c24ae460fc8dc922f73e810d127562883
leandrotune <leandro.tune@hotmail.com>
2024-03-22 22:58:16 -0300

    style: I indented the eslint code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter, Nunito } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
 
-const nunito = Nunito({ subsets: ["latin"] });
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "TypeWeather",
-  description: "weather forecast project",
-};
+  title: 'TypeWeather',
+  description: 'weather forecast project',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={nunito.className}>{children}</body>
     </html>
-  );
+  )
 }
