fa93414a6340e1cb3cc3eb67147fc43bb2a6fa00
leandrotune <leandro.tune@hotmail.com>
2024-03-24 13:59:46 -0300

    feat: add component inputSearch

---
+++ src/components/input-search.tsx
@@ -0,0 +1,9 @@
+export function InputSearch() {
+  return (
+    <input
+      type="search"
+      placeholder="Buscar local"
+      className="rounded-lg bg-gray-600 text-gray-400 flex-1 h-14 px-5 outline-none"
+    />
+  )
+}
